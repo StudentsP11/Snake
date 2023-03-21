64945e4b07cbc90c378c9d870cecd525f5ea998d
ilnazmen <ilnaz.cynopm@mail.ru>
2023-03-21 21:27:05 +0300

    WIP

---
+++ GameObject.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Point.h"
+
+class Object
+{
+	using point = vec2d<size_t>;
+public:
+private:
+	
+};
---
+++ Point.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template <typename T>
+struct vec2d
+{
+	T x, y;
+};
